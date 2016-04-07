58fd2e3430d0df83116a18996e0f2c9401bf63c4
ebapire <eva.garcia.domingo@gmail.com>
2016-04-07 15:37:29 +0200

    archivos del proyect terminados

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^$', 'ejercicios.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^CMS/nuevo/(.*)/(.*)', 'CMS.views.nuevo'),
+    url(r'^CMS/show/$', 'CMS.views.show'),
     url(r'^admin/', include(admin.site.urls)),
 )
